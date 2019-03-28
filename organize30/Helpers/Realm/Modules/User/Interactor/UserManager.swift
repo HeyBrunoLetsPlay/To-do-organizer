067a8e4f40235a0f1bb5215721c60c0b03e9b918
Bruno Raphael <bruno.castilho@stormsec.com.br>
2019-03-28 01:27:30 -0300

    [ADD] Rename? Workspace (sim)

--- organize30/Helpers/Realm/Modules/User/Interactor/UserManager.swift
+++ organize30/Helpers/Realm/Modules/User/Interactor/UserManager.swift
@@ -11,12 +11,12 @@ import RealmSwift
 class UserManager {
     func save(image:String, name:String, genre:String){
         let realmManager = RealmManager()
-        if let objUser = realmManager.getObjects(type:User.self)?.first as? User {
-            let editUser = User.init(id:objUser.id, image:image, name:name, genre:genre)
+        if let objUser = realmManager.getObjects(type:UserEntity.self)?.first as? UserEntity {
+            let editUser = UserEntity.init(id:objUser.id, image:image, name:name, genre:genre)
             realmManager.editObjects(objs:editUser)
         }else {
-            let id = realmManager.incrementID(type:User.self)
-            let obj = User.init(id:id, image:image, name:name, genre:genre)
+            let id = realmManager.incrementID(type:UserEntity.self)
+            let obj = UserEntity.init(id:id, image:image, name:name, genre:genre)
             realmManager.saveObjects(objs:obj)
         }
     }
